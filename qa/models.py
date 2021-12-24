935f7c15c0baf313a178e7809d1cf7f4c9086660
mikhail.khaykin <m.khaykin@outlook.com>
2021-12-24 07:46:32 +0300

    task 2.5

--- qa/models.py
+++ qa/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 class QuestionManager(models.Manager):
     def new(self):
-        return self.order_by('-id')
+        return self.order_by('-pk')     # -id
 
     def popular(self):
         return self.order_by('-rating')
